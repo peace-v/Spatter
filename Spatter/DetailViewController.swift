9fa1ff93f3d78c3663077cf4962d58dad1657f9d
peace-v <1184846145@qq.com>
2015-12-26 17:19:57 +0800

    finish detailView basic part

--- Spatter/DetailViewController.swift
+++ Spatter/DetailViewController.swift
@@ -8,42 +8,47 @@
 
 import UIKit
 import CoreMotion
+import Whisper
 
 class DetailViewController: UIViewController {
 	let image = UIImage(named: "space")
 	var imagePanViewController = SCImagePanViewController()
+    var infoBtnPopTipView = CMPopTipView()
 
 	@IBOutlet weak var toolbar: UIToolbar!
 	@IBOutlet weak var infoButton: UIBarButtonItem!
 
 	@IBAction func back(sender: AnyObject) {
-//		self.dismissViewControllerAnimated(true, completion: nil)
-        self.navigationController!.popViewControllerAnimated(true)
+		self.navigationController!.popViewControllerAnimated(true)
 	}
 	@IBAction func saveToAlbum(sender: AnyObject) {
-		UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+        UIImageWriteToSavedPhotosAlbum(image!, self, "image:didFinishSavingWithError:contextInfo:", nil)
 	}
 	@IBAction func likePhoto(sender: AnyObject) {
 	}
 	@IBAction func sharePhoto(sender: AnyObject) {
+        let activityViewController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        self.presentViewController(activityViewController, animated: true, completion: nil)
 	}
 	@IBAction func showPhotoInfo(sender: AnyObject) {
+        infoBtnPopTipView.message = "Creator Info"
+        infoBtnPopTipView.presentPointingAtBarButtonItem(infoButton, animated: true)
 	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		// Do any additional setup after loading the view.
-        
-        self.navigationController!.navigationBarHidden = true
-        
-        // transparent toolbar
+		self.navigationController!.navigationBarHidden = true
+
+		// transparent toolbar
 		self.toolbar.setBackgroundImage(UIImage(),
 			forToolbarPosition: UIBarPosition.Any,
 			barMetrics: UIBarMetrics.Default)
 		self.toolbar.setShadowImage(UIImage(),
 			forToolbarPosition: UIBarPosition.Any)
 
+        // add motionView
 		let motionManager = CMMotionManager()
 		imagePanViewController = SCImagePanViewController(motionManager: motionManager)
 		imagePanViewController.willMoveToParentViewController(self)
@@ -59,7 +64,10 @@ class DetailViewController: UIViewController {
 		imagePanViewController.configureWithImage(UIImage(named: "space")!)
 
 		// init poplabel
-
+        infoBtnPopTipView.dismissTapAnywhere = true
+        infoBtnPopTipView.backgroundColor = UIColor.blackColor()
+        infoBtnPopTipView.textColor = UIColor.whiteColor()
+        infoBtnPopTipView.has3DStyle = false
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -77,4 +85,15 @@ class DetailViewController: UIViewController {
 	 // Pass the selected object to the new view controller.
 	 }
 	 */
+
+	// MARK: Whisper
+    func image(image: UIImage, didFinishSavingWithError error: NSError?, contextInfo:UnsafePointer<Void>) {
+        if error == nil {
+            let murmur = Murmur(title: "Image Saved")
+            Whistle(murmur)
+        }else {
+            let murmur = Murmur(title: "Failed to save image, please try again.")
+            Whistle(murmur)
+        }
+    }
 }
