bea6ee8a1b6fc05dda99f2189a7141cf80365315
peace-v <1184846145@qq.com>
2017-07-17 20:22:48 +0800

    添加 pod

---
+++ iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/molay/Desktop/my\ app/Spatter/iOSInjectionProject/BundleContents.m \
+  /tmp/injectionforxcode/BundleInjection.h \
+  /tmp/injectionforxcode/BundleInterface.h \
+  /Users/molay/Desktop/my\ app/Spatter/iOSInjectionProject/BundleContents.h
